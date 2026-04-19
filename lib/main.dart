42086987caf7e82fb345d8c1e55a58209e7ae505
Ronnie <kayronnie91@gmail.com>
2026-04-19 21:02:48 +0700

    create rest timer card

--- lib/main.dart
+++ lib/main.dart
@@ -51,8 +51,55 @@ class MyApp extends StatelessWidget {
 
               // SUBHEADING //
               SizedBox(height:8),
+              Text('WEEK 4 - DAY 2',
+              style:TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight(400),
+                color:Color(0xFFABABA6),
+              ),
+              ),
+              SizedBox(height: 18),
 
-              Text('WEEK 4 - DAY 2'),
+              // REST TIMER CONTAINER //
+
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors:[
+                      Color(0xFF2A2F26),
+                      Color(0xFF1C1F1A),
+                    ]
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('01:45',
+                    style:TextStyle(
+                      fontSize: 48,
+                      color:Color(0xFFDFFF9F),
+                    )
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.timer,
+                        color: Colors.white24,
+                        ),
+                        SizedBox(width:4),
+                        Text('REST TIMER',
+                        style: TextStyle(
+                          color:Colors.white24,
+                          fontWeight: FontWeight(700),
+                        ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
 
             ],
           ),
